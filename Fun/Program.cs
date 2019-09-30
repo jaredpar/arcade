16fb9c2d58e3dda679f21b42fbf314c2299b5e60
Jared Parsons <jaredpparsons@gmail.com>
2019-09-30 13:35:54 -0700

    Fan out

--- Fun/Program.cs
+++ Fun/Program.cs
@@ -6,13 +6,23 @@ using System.Threading.Tasks;
 using System.Linq;
 using System.Collections.Generic;
 using System.Reflection.Metadata;
+using RunTests;
+using Microsoft.WindowsAzure.Storage.File.Protocol;
+using Microsoft.Data.Edm.Library;
+using System.Runtime.CompilerServices;
 
 namespace Fun
 {
     internal static class Program
     {
+        // private static readonly string TestResourceDllName = "Microsoft.CodeAnalysis.Test.Resources.Proprietary.dll";
+
         internal static async Task Main(string[] args)
         {
+            await QueueSemantics().ConfigureAwait(false);
+
+
+            /*
             var (helixApi, job) = QueueUnitTests();
             var sentJobTask = job.SendAsync(Console.WriteLine);
             var sentJob = await sentJobTask.ConfigureAwait(false);
@@ -22,11 +32,10 @@ namespace Fun
             Console.WriteLine(sentJob.ResultsContainerUri + sentJob.ResultsContainerReadSAS);
 
             var start = DateTime.UtcNow;
-            await WaitForJobAsync(helixApi.Job, sentJob.CorrelationId);
+            await helixApi.Job.WaitForJobAsync(sentJob.CorrelationId).ConfigureAwait(false);
 
             Console.WriteLine($"Execution Took {DateTime.UtcNow - start}");
 
-            /*
             var token = await GetToken("helix").ConfigureAwait(false);
             var api = ApiFactory.GetAuthenticated(token);
             var source = "test/payload";
@@ -51,6 +60,26 @@ namespace Fun
             */
         }
 
+        private static async Task Run(IHelixApi helixApi, IJobDefinition job)
+        {
+            var sendStart = DateTime.UtcNow; 
+            var sentJobTask = job.SendAsync(Console.WriteLine);
+            var sentJob = await sentJobTask.ConfigureAwait(false);
+            var sendEnd = DateTime.UtcNow;
+            Console.WriteLine(sentJob.CorrelationId);
+            Console.WriteLine(sentJob.ResultsContainerUri);
+            Console.WriteLine(sentJob.ResultsContainerReadSAS);
+            Console.WriteLine(sentJob.ResultsContainerUri + sentJob.ResultsContainerReadSAS);
+
+            var executionStart = DateTime.UtcNow;
+            await helixApi.Job.WaitForJobAsync(sentJob.CorrelationId).ConfigureAwait(false);
+            var executionEnd = DateTime.UtcNow;
+
+            Console.WriteLine($"Upload Took {sendEnd - sendStart}");
+            Console.WriteLine($"Execution Took {executionEnd - executionStart}");
+            Console.WriteLine($"Total time {executionEnd - sendStart}");
+        }
+
         internal static (IHelixApi HelixApi, IJobDefinition JobDefinition) QueueUnitTests()
         {
             var api = ApiFactory.GetAnonymous();
@@ -58,17 +87,19 @@ namespace Fun
                 .Job
                 .Define()
                 .WithType("test/unit")
+                // .WithTargetQueue("Windows.10.Amd64.Open")
                 .WithTargetQueue("Windows.10.Amd64.ClientRS5.Open")
                 .WithSource("RoslynUnitTests")
                 .WithCreator("jaredpar");
 
+            var scheduler = new AssemblyScheduler(methodLimit: 500);
             foreach (var directoryRoot in Directory.EnumerateDirectories(@"P:\roslyn\artifacts\bin", "*CSharp*.UnitTests"))
             {
-                var directory = Path.Combine(directoryRoot, @"Debug/net472");
+                var directory = Path.Combine(directoryRoot, @"Debug\net472");
                 if (Directory.Exists(directory))
                 {
                     Console.Write($"Queueing {directory} ... ");
-                    job = QueueUnitTest(job, directory);
+                    job = QueueUnitTest(job, scheduler, directory);
                     Console.WriteLine("Done");
                 }
             }
@@ -76,30 +107,85 @@ namespace Fun
             return (api, job);
         }
 
-        internal static IJobDefinition QueueUnitTest(IJobDefinition job, string unitTestDirectory)
+        internal static IJobDefinition QueueUnitTest(IJobDefinition job, AssemblyScheduler scheduler, string unitTestDirectory)
         {
-            prepXunit();
+            PrepXunit();
 
             var unitTestFilePath = Directory.EnumerateFiles(unitTestDirectory, "*.UnitTests.dll").Single();
             var unitTestFileName = Path.GetFileName(unitTestFilePath);
-            var uploadEnvironmentName = "%HELIX_WORKITEM_UPLOAD_ROOT%";
-            var xmlFilePath = @$"{uploadEnvironmentName}\{unitTestFileName}.xml";
-            var htmlFilePath = @$"{uploadEnvironmentName}\{unitTestFileName}.html";
+            IEnumerable<AssemblyPartitionInfo> partitions;
 
-            var name = Path.GetDirectoryName(unitTestDirectory);
+            if (unitTestFileName == "Microsoft.CodeAnalysis.CSharp.Emit.UnitTests.dll" ||
+                unitTestFileName == "Microsoft.CodeAnalysis.CSharp.Semantic.UnitTests.dll" ||
+                unitTestFileName == "Microsoft.CodeAnalysis.EditorFeatures.UnitTests.dll" ||
+                unitTestFileName == "Microsoft.CodeAnalysis.EditorFeatures2.UnitTests.dll" ||
+                unitTestFileName == "Microsoft.VisualStudio.LanguageServices.UnitTests.dll" ||
+                unitTestFileName == "Microsoft.CodeAnalysis.CSharp.EditorFeatures.UnitTests.dll" ||
+                unitTestFileName == "Microsoft.CodeAnalysis.VisualBasic.EditorFeatures.UnitTests.dll")
+            {
+                partitions = scheduler.Schedule(unitTestFilePath);
+            }
+            else
+            {
+                partitions = new[] { new AssemblyPartitionInfo(unitTestFilePath) };
+            }
 
-            var batchFileName = "xunit.cmd";
-            var batchContent = @$".\xunit.console.exe {unitTestFileName} -html {htmlFilePath} -xml {xmlFilePath}";
-            File.WriteAllText(Path.Combine(unitTestDirectory, batchFileName), batchContent);
+            var partitionId = 0;
+            foreach (var info in partitions)
+            {
+                job = QueueOne(job, info, partitionId);
+                partitionId++;
+            }
 
-            return job
-                .DefineWorkItem(unitTestFileName.Replace('.', '-'))
-                .WithCommand(@$"cmd /c {batchFileName}")
-                .WithDirectoryPayload(unitTestDirectory)
-                .WithTimeout(TimeSpan.FromMinutes(15))
-                .AttachToJob();
+            return job;
 
-            void prepXunit()
+            IJobDefinition QueueOne(IJobDefinition job, AssemblyPartitionInfo info, int partitionId)
+            {
+                var displayName = Path.GetFileNameWithoutExtension(unitTestFileName) + $".{partitionId}";
+                var batchFileName = EnsureBatchFile(info, partitionId);
+                return job
+                    .DefineWorkItem(displayName)
+                    .WithCommand(@$"cmd /c {batchFileName}")
+                    .WithDirectoryPayload(unitTestDirectory)
+                    .WithTimeout(TimeSpan.FromMinutes(15))
+                    .AttachToJob();
+            }
+
+            string EnsureBatchFile(AssemblyPartitionInfo info, int partitionId)
+            {
+                var uploadEnvironmentName = "%HELIX_WORKITEM_UPLOAD_ROOT%";
+                var partitionDirectoryName = $"Partition{partitionId}";
+                var xmlFilePath = @$"{uploadEnvironmentName}\{partitionDirectoryName}\{unitTestFileName}.xml";
+                var htmlFilePath = @$"{uploadEnvironmentName}\{partitionDirectoryName}\{unitTestFileName}.html";
+
+                var name = Path.GetDirectoryName(unitTestDirectory);
+                var batchFileName = $"xunit-{partitionId}.cmd";
+                var batchFilePath = Path.Combine(unitTestDirectory, batchFileName);
+                var batchContent = @$".\xunit.console.exe {unitTestFileName} -html {htmlFilePath} -xml {xmlFilePath} {info.ClassListArgumentString}";
+
+                try
+                {
+                    if (File.Exists(batchFilePath))
+                    {
+                        var currentContet = File.ReadAllText(batchFilePath);
+                        if (currentContet == batchContent)
+                        {
+                            // don't screw up payload caching by modifying the directory if the batch file has already
+                            // been laid down
+                            return batchFileName;
+                        }
+                    }
+                }
+                catch
+                {
+                    // Error reading the file, no problem, just rewrite it with current content
+                }
+
+                File.WriteAllText(Path.Combine(unitTestDirectory, batchFileName), batchContent);
+                return batchFileName;
+            }
+
+            void PrepXunit()
             {
                 var xunitToolsDirectory = @"P:\nuget\xunit.runner.console\2.4.1\tools\net472";
                 foreach (var sourceFilePath in Directory.EnumerateFiles(xunitToolsDirectory))
@@ -111,55 +197,108 @@ namespace Fun
             }
         }
 
-
-        private static async Task WaitForJobAsync(IJob job, string correlationId)
+        private static async Task QueueSemantics()
         {
-            do
+            const string unitTestFilePath = @"P:\roslyn\artifacts\bin\Microsoft.CodeAnalysis.CSharp.Emit.UnitTests\Debug\net472\Microsoft.CodeAnalysis.CSharp.Emit.UnitTests.dll";
+            var unitTestFileName = Path.GetFileName(unitTestFilePath);
+            var unitTestDirectory = Path.GetDirectoryName(unitTestFilePath);
+
+            var scheduler = new AssemblyScheduler(methodLimit: 50);
+            var batchDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(batchDirectory);
+            var batchFileNames = new List<string>();
+            int partitionId = 0;
+            foreach (var info in scheduler.Schedule(unitTestFilePath))
             {
-                try
+                batchFileNames.Add(EnsureBatchFile(batchDirectory, info, GetPartitionId(partitionId)));
+                partitionId++;
+            }
+
+            PrepXunit();
+
+            var api = ApiFactory.GetAnonymous();
+            var job = api
+                .Job
+                .Define()
+                .WithType("test/unit")
+                // .WithTargetQueue("Windows.10.Amd64.Open")
+                .WithTargetQueue("Windows.10.Amd64.ClientRS5.Open")
+                .WithSource("RoslynUnitTests")
+                .WithCreator("jaredpar")
+                .WithCorrelationPayloadDirectory(unitTestDirectory);
+
+            for (int i = 0; i < batchFileNames.Count; i++)
+            {
+                var batchFileName = batchFileNames[i];
+                var displayName = Path.GetFileNameWithoutExtension(unitTestFileName) + "." + GetPartitionId(i);
+                job = job
+                    .DefineWorkItem(displayName)
+                    .WithCommand(@$"cmd /c {batchFileName}")
+                    .WithDirectoryPayload(batchDirectory)
+                    .WithTimeout(TimeSpan.FromMinutes(15))
+                    .AttachToJob();
+            }
+
+            Console.WriteLine($"Partition count is {batchFileNames.Count}");
+            await Run(api, job).ConfigureAwait(false);
+
+            static string GetPartitionId(int id) => id.ToString("D3");
+
+            static string EnsureBatchFile(string batchDirectory, AssemblyPartitionInfo info, string partitionId)
+            {
+                var unitTestFileName = Path.GetFileName(info.AssemblyPath);
+                var uploadEnvironmentName = "%HELIX_WORKITEM_UPLOAD_ROOT%";
+                var xmlFilePath = @$"{uploadEnvironmentName}\{unitTestFileName}.{partitionId}.xml";
+                var htmlFilePath = @$"{uploadEnvironmentName}\{unitTestFileName}.{partitionId}.html";
+
+                var batchFileName = $"xunit-{partitionId}.cmd";
+                var batchFilePath = Path.Combine(batchDirectory, batchFileName);
+                var batchContent = @$"
+cd %HELIX_CORRELATION_PAYLOAD%
+.\xunit.console.exe {unitTestFileName} -html {htmlFilePath} -xml {xmlFilePath} {info.ClassListArgumentString}";
+
+                WriteFileContentIfDifferent(batchFilePath, batchContent);
+                return batchFileName;
+            }
+
+            void PrepXunit()
+            {
+                var xunitToolsDirectory = @"P:\nuget\xunit.runner.console\2.4.1\tools\net472";
+                foreach (var sourceFilePath in Directory.EnumerateFiles(xunitToolsDirectory))
                 {
-                    await job.WaitForJobAsync(correlationId).ConfigureAwait(false);
-                    break;
-                }
-                catch (RestApiException ex)
-                {
-                    if (ex.Response.StatusCode == HttpStatusCode.InternalServerError ||
-                        ex.Response.StatusCode == HttpStatusCode.ServiceUnavailable)
-                    {
-                        await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    var destFileName = Path.GetFileName(sourceFilePath);
+                    var destFilePath = Path.Combine(unitTestDirectory, destFileName);
+                    File.Copy(sourceFilePath, destFilePath, overwrite: true);
                 }
             }
-            while (true);
         }
 
-        private static string GetConsoleUrl(string source, string correlationId, string accessToken)
+        /// <summary>
+        /// Write <paramref name="contents"/> to <paramref name="filePath"/> if they are different from the
+        /// current contents of the file. Returns true if the file was written to disk.
+        /// </summary>
+        private static bool WriteFileContentIfDifferent(string filePath, string content)
         {
-            source = Uri.EscapeDataString(source);
-            if (accessToken is object)
+            try
             {
-                accessToken = $"?access_token={accessToken}";
-            }
-            return $"https://helix.dot.net/api/2019-06-17/jobs/{correlationId}/workitems/{source}/console{accessToken}";
-        }
-
-        internal static async Task<string> GetToken(string name)
-        {
-            var lines = await File.ReadAllLinesAsync(@"p:\tokens.txt").ConfigureAwait(false);
-            foreach (var line in lines)
-            {
-                var items = line.Split(new[] { ':' }, count: 2, StringSplitOptions.RemoveEmptyEntries);
-                if (items[0] == name)
+                if (File.Exists(filePath))
                 {
-                    return items[1];
+                    var currentContent = File.ReadAllText(filePath);
+                    if (currentContent == content)
+                    {
+                        // don't screw up payload caching by modifying the directory if the batch file has already
+                        // been laid down
+                        return false;
+                    }
                 }
             }
+            catch
+            {
+                // Error reading the file, no problem, just rewrite it with current content
+            }
 
-            throw new Exception($"Could not find token with name {name}");
+            File.WriteAllText(filePath, content);
+            return true;
         }
     }
 }
